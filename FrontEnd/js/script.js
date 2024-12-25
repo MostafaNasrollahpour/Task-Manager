1d7c47d7558189e316215e1c2dd863b3bef196d8
Mostafa Nasrollahpour <iammostafanasrollahpour@gmail.com>
2024-12-25 22:34:17 +0330

    this is for raziyeh

---
+++ FrontEnd/js/script.js
@@ -0,0 +1,48 @@
+
+const validateEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
+
+
+// document.getElementById('myForm').addEventListener('submit', function (e) {
+//     e.preventDefault();
+// });
+
+function check_inputs() {
+    const inputs = document.querySelectorAll('.in');
+    let allValid = true;
+
+    inputs.forEach(input => {
+        if (input.value.trim() === "") {
+            allValid = false;
+            input.style.border = "2px solid red";
+        } else {
+            input.style.border = "1px solid #ccc";
+        }
+    });
+
+    if (!allValid) {
+        alert("Please fill out all fields.");
+        return false;
+    }
+
+    const pass = document.getElementById('for-pass').value;
+    const rePass = document.getElementById('for-re-pass').value;
+    if (pass !== rePass) {
+        alert("Passwords do not match");
+        return false;
+    }
+
+    const email = document.getElementById('for-email').value
+    if (!validateEmail(email)) {
+        alert("Invalid Email.")
+        return false
+    }
+
+
+    // window.location.href = "firstPage.html";
+}
