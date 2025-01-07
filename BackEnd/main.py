cbb543713cfc9e41d50484ca46736db4b1e667d1
Mostafa Nasrollahpour <iammostafanasrollahpour@gmail.com>
2025-01-07 10:15:16 +0330

    add cors and allow front use my api

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,9 +1,11 @@
 # import section about library
+import datetime
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 # import section about my defind
 from schemas.users import UserSignIn, UserSignUp
+from schemas.projects import ProjectCreated
 from schemas.response import *
 from database import *
 
@@ -11,6 +13,25 @@ from database import *
 
 app = FastAPI()
 
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+@app.get('/')
+async def home_page():
+    return {
+        "message": 'hello world'
+    }
 
 
 @app.post('/signin')
@@ -41,3 +62,5 @@ async def user_signup(user: UserSignUp):
     if is_password:
         return OK
     return IncorrectPassword
+
+
