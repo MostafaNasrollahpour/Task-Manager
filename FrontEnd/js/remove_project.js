518cecacdcf4e507e340e26c6768f26cbbc6758e
Mostafa Nasrollahpour <iammostafanasrollahpour@gmail.com>
2025-01-11 03:15:25 +0330

    fetch all current user's projects that he's their manager

--- FrontEnd/js/remove_project.js
+++ FrontEnd/js/remove_project.js
@@ -1,23 +1,61 @@
-const items = ['raziyeh', 'mostafa', 'mahtab', 'fgjhfog'];
+async function sendData(data) {
+    try {
+        const response = await fetch('http://127.0.0.1:8000/get-my-users-projects', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        });
 
-items.forEach(item => {
-    const div = document.createElement('div');
-    div.className = 'card';
-    
-    // Create the card content/
-    const cardContent = `
-        <div>
-            <h5 class="for-admin">${item}</h5>
-            <div class="d-flex justify-content-end">
-                <button type="button" class="remove-btn btn btn-danger" style="border:none;" data-name="${item}">Remove</button>
-            </div>
-        </div>
-    `;
-    
-    div.innerHTML = cardContent;
-    
-    container.appendChild(div);
-});
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+}
+
+async function main() {
+    try {
+        let currentUser = localStorage.getItem('currentUser');
+        currentUser = JSON.parse(currentUser);
+
+        const result = await sendData(currentUser);
+
+        const projects = result.projects;
+
+        projects.forEach(item => {
+            const div = document.createElement('div');
+            div.className = 'card';
+            
+            // Create the card content/
+            const cardContent = `
+                <div>
+                    <h5 class="for-admin">${item.name}</h5>
+                    <div class="d-flex justify-content-end">
+                        <button type="button" class="remove-btn btn btn-danger" style="border:none;" data-name="${item}">Remove</button>
+                    </div>
+                </div>
+            `;
+            
+            div.innerHTML = cardContent;
+            
+            container.appendChild(div);
+        });
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+main();
+
+
 
 // Add click event listener to all buttons
 document.addEventListener('click', function(event) {
