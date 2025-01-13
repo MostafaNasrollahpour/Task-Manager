6118646646f8d0b200a4a2d788bdf9f6e976ee40
Mostafa Nasrollahpour <iammostafanasrollahpour@gmail.com>
2025-01-13 20:49:08 +0330

    front of remove_projects finished

--- FrontEnd/js/remove_project.js
+++ FrontEnd/js/remove_project.js
@@ -23,22 +23,27 @@ async function sendData(data) {
 async function main() {
     try {
         let currentUser = localStorage.getItem('currentUser');
-        currentUser = JSON.parse(currentUser);
+        currentUser = JSON.parse('{"email":"aryan"}');
 
         const result = await sendData(currentUser);
 
         const projects = result.projects;
-
-        projects.forEach(item => {
+        console.log(projects)
+        projects.forEach(project => {
             const div = document.createElement('div');
             div.className = 'card';
             
             // Create the card content/
             const cardContent = `
                 <div>
-                    <h5 class="for-admin">${item.name}</h5>
+                    <h5 class="for-admin">Project Name: ${project.name}</h5>
+                    <h5 class="for-admin">Receiver: ${project.worker}</h5>
+                    <h5 class="for-admin">Start Date: ${project.start_date}</h5>
+                    <h5 class="for-admin">End Date: ${project.end_date}</h5>
+                    <h5 class="for-admin">Description: ${project.description}</h5>
+                    <h5 class="for-admin">Priority: ${project.priority}</h5>
                     <div class="d-flex justify-content-end">
-                        <button type="button" class="remove-btn btn btn-danger" style="border:none;" data-name="${item}">Remove</button>
+                        <button type="button" class="remove-btn btn btn-danger" style="border:none;" data-name="${project.id}">Remove</button>
                     </div>
                 </div>
             `;
